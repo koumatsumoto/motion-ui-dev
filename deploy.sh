7ad3a17afc3caffd0c99ae1613e0323a16621aeb
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-12 01:20:12 +0900

    build: fix deploy script (#15)

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ WORKING_BRANCH=$(git rev-parse --abbrev-ref HEAD);
 DEPLOY_BRANCH=deploy;
 DEPLOY_ORIGIN=deploy;
 ORIGIN_CHECK=$(git remote -v | sed -n 's/^deploy\s//p');
+BRANCH_CHECK=$(git show-branch --list | sed -n 's/\[deploy]//p');
 
 # if origin is not registered, exit
 if [ -z "$ORIGIN_CHECK" ]; then
@@ -16,8 +17,11 @@ if [ -z "$ORIGIN_CHECK" ]; then
   exit 1;
 fi
 
-# prepare temporary branch
-git branch -D $DEPLOY_BRANCH;
+# if branch already exists, delete it
+if [ -n "$BRANCH_CHECK" ]; then
+  git branch -D $DEPLOY_BRANCH;
+fi
+# checkout from current working branch
 git checkout -b $DEPLOY_BRANCH;
 # build js file
 yarn build:prod;
